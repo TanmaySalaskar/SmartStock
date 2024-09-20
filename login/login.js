02944bdf8a5ab0af7ef04a4d233ccb52614c6143
TanmaySalaskar <78720317+TanmaySalaskar@users.noreply.github.com>
2024-09-21 04:24:12 +0530

    Add files via upload

--- login/login.js
+++ login/login.js
@@ -3,12 +3,16 @@ document.getElementById('login-form').addEventListener('submit', function(event)
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
 
-    // Basic validation (you can add more complex validation here)
-    if (username === 'admin' && password === 'password123') {
+    // Retrieve stored credentials
+    const storedUsername = localStorage.getItem('username');
+    const storedPassword = localStorage.getItem('password');
+
+    // Check against stored credentials
+    if (username === storedUsername && password === storedPassword) {
         window.location.href = '/page/page.html'; // Redirect to the main page after successful login
     } else {
-        const errorMessage = document.getElementById('error-message');
         errorMessage.textContent = 'Invalid username or password';
         errorMessage.style.display = 'block';
     }
